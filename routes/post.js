79e27c97a94d1212429cb8474198b6b17caceba3
BoTung <bver49@gmail.com>
2016-07-02 01:22:42 +0800

    add rest

--- routes/post.js
+++ routes/post.js
@@ -2,12 +2,43 @@ var express = require('express');
 var router = express.Router();
 var database =  require('./firebase');
 
+/* index */
 router.get('/', function(req, res) {
   console.log("GET '/post'");
   database.ref('post').on('value', function(snapshot) {
     var data = snapshot.val();
     res.render('post/index',{"data":data});
   });
+});
+
+/* show */
+router.get('/:id', function(req, res) {
+  res.render('post/show');
+});
+
+/* new */
+router.get('/new', function(req, res) {
+  res.render('post/new');
+});
+
+/* create */
+router.post('/create', function(req, res) {
+
+});
+
+/* edit */
+router.get('/edit', function(req, res) {
+
+});
+
+/* update */
+router.post('/update', function(req, res) {
+
+});
+
+/* del */
+router.delete('/:id', function(req,res) {
+
 });
 
 module.exports = router;

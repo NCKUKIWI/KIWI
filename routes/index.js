2852976fdfac86238116b70988d2422a3d7ab4d3
nick0916 <nickliao2013@gmail.com>
2016-07-05 02:54:39 +0800

    finish catalog query function

--- routes/index.js
+++ routes/index.js
@@ -6,69 +6,47 @@ var db = require('../model/db');
 router.get('/', function(req, res) {
   console.log("GET '/'");
   db.getall('post',function(datas){
-  	var teacher = search_item(datas, "teacher");
-	  var courseName = search_item(datas, "course_name");
 
-	  if(!req.query.hasOwnProperty("teacher") && !req.query.hasOwnProperty("course")){
+	  if(req.query.hasOwnProperty("teacher")){
+	  	query_post(datas, req.query.teacher, "teacher");
+	  }
+	  else if(req.query.hasOwnProperty("course_name")){
+	  	query_post(datas, req.query.course_name, "course_name");
+	  }
+	  else if(req.query.hasOwnProperty("catalog")){
+	  	switch(req.query.catalog){
+	  		case "A9": 
+	  			req.query.catalog = "通識" 
+	  			break;
+	  		case "A2": 
+	  			req.query.catalog = "體育" 
+	  			break;
+	  		case "A1": 
+	  			req.query.catalog = "外國語言" 
+	  			break;
+	  		case "A6": 
+	  			req.query.catalog = "服務學習" 
+	  			break;
+	  		case "A7":
+	  			req.query.catalog = "基礎國文"
+	  			break;
+	  		case "AG": 
+	  			req.query.catalog = "公民歷史" 
+	  			break;
+	  	}
+	  	query_post(datas, req.query.catalog, "catalog");
+	  }
+	  else{
+			var teacher = search_item(datas, "teacher");
+		  var courseName = search_item(datas, "course_name");
+
 	    res.render('post/index',{
 	    	'data':datas,
 	    	'teachers': teacher,
 	    	'course_name': courseName
 	    });
 	  }
-	  else if(req.query.hasOwnProperty("teacher")){
-	  	var query_data = [];
-	  	for(var i in datas){
-	  		if(datas[i].teacher.match(req.query.teacher)){
-	  			query_data.push(datas[i]);
-	  		}
-	  	}
-	    res.render('post/index',{
-	    	'data':query_data,
-	    	'teachers': teacher,
-	    	'course_name': courseName
-	    });
-
-		  // db.findbyTeacher('post', req.query.teacher,function(datas){
-
-		  //   res.render('post/index',{
-		  //   	'data':datas,
-		  //   	'teachers': teacher,
-		  //   	'course_name': courseName
-		  //   });
-		  // });
-	  }
-	  else if(req.query.hasOwnProperty("course")){
-			var regex = req.query.course.replace(/\(/g, "\\(");
-			regex = regex.replace(/\)/g, "\\)");
-			regex = regex.replace(/\./g, "\\.");
-
-	  	var query_data = [];
-	  	for(var i in datas){
-	  		if(datas[i].course_name.match(regex)){
-	  			query_data.push(datas[i]);
-	  		}
-	  	}
-	    res.render('post/index',{
-	    	'data':query_data,
-	    	'teachers': teacher,
-	    	'course_name': courseName
-	    });
-
-
-		  // db.findbyCourseName('post', req.query.course,function(datas){
-
-		  //   res.render('post/index',{
-		  //   	'data':datas,
-		  //   	'teachers': teacher,
-		  //   	'course_name': courseName
-		  //   });
-		  // });
-	  }
   });
-
-
-
 
 function search_item(datas, item){
   	var item_array = [];
@@ -91,6 +69,29 @@ function search_item(datas, item){
 			}
 		}
 		return item_array;
+}
+
+function query_post(datas, req, item){
+	var teacher = search_item(datas, "teacher");
+  var courseName = search_item(datas, "course_name");
+
+	var regex = req.replace(/\(/g, "\\(");
+	regex = regex.replace(/\)/g, "\\)");
+	regex = regex.replace(/\./g, "\\.");
+
+	var query_data = [];
+	for(var i in datas){
+		var data = datas[i]
+		if(data[item].match(regex)){
+			query_data.push(datas[i]);
+		}
+	}
+
+  res.render('post/index',{
+  	'data':query_data,
+  	'teachers': teacher,
+  	'course_name': courseName
+  });
 }
 
 

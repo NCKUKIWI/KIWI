14a699111d6ac1ee03ad761ca77498e990fa9c20
nick0916 <nickliao2013@gmail.com>
2016-07-04 16:16:49 +0800

    update query button

---
+++ config.js
@@ -0,0 +1,1 @@
+module.exports = {'dburl':'postgres://cjcbnetdyvsggi:bGx_RHEO8k2v8bslCnji_gh9VH@ec2-54-235-65-139.compute-1.amazonaws.com:5432/dchn8biep7catf?ssl=true'};
--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,41 @@ var db = require('../model/db');
 /* root */
 router.get('/', function(req, res) {
   console.log("GET '/'");
-  db.getall('post',function(datas){
-    res.render('post/index',{'data':datas});
-  });
+  // if(!req.query.hasOwnProperty("teacher") && !req.query.hasOwnProperty("course")){
+	  db.getall('post',function(datas){
+	  	// var teacher = search_item(datas, "teacher");
+	  	// var className = search_item(datas, "class_name");
+
+
+	    res.render('post/index',{
+	    	'data':datas,
+	    	// 'teacher': teacher,
+	    	// 'class_name': className,
+	    });
+	  });
+  // }
+
+function search_item(datas, item){
+  	var item_array = [];
+  	var data = datas[0]
+		item_array.push(data[item]);
+
+		for(var i in datas){
+			data = datas[i];
+			for(var j in item_array){
+				if(data[item] == null) continue;
+				data[item] = data[item].replace(/\.|\(|\)/g, " ");
+				if(item_array[j].match(data[item]) != null)
+					break;
+				else if (j == item_array.length - 1){
+					item_array.push(data[item]);
+				}
+			}
+		}
+}
+
+
+
 });
 
 module.exports = router;

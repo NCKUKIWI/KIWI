2a76c96d00a08eefa67ca0674ec366f57343085e
BoTung <bver49@gmail.com>
2016-07-08 00:30:18 +0800

    add ajax order post

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,80 +1,144 @@
+$(document).ready(function(){
+  function urlObject(options) {
 
-function urlObject(options) {
+      var url_search_arr,
+          option_key,
+          i,
+          urlObj,
+          get_param,
+          key,
+          val,
+          url_query,
+          url_get_params = {},
+          a = document.createElement('a'),
+          default_options = {
+              'url': window.location.href,
+              'unescape': true,
+              'convert_num': true
+          };
 
-    var url_search_arr,
-        option_key,
-        i,
-        urlObj,
-        get_param,
-        key,
-        val,
-        url_query,
-        url_get_params = {},
-        a = document.createElement('a'),
-        default_options = {
-            'url': window.location.href,
-            'unescape': true,
-            'convert_num': true
-        };
+      if (typeof options !== "object") {
+          options = default_options;
+      } else {
+          for (option_key in default_options) {
+              if (default_options.hasOwnProperty(option_key)) {
+                  if (options[option_key] === undefined) {
+                      options[option_key] = default_options[option_key];
+                  }
+              }
+          }
+      }
 
-    if (typeof options !== "object") {
-        options = default_options;
-    } else {
-        for (option_key in default_options) {
-            if (default_options.hasOwnProperty(option_key)) {
-                if (options[option_key] === undefined) {
-                    options[option_key] = default_options[option_key];
-                }
-            }
+      a.href = options.url;
+      url_query = a.search.substring(1);
+      url_search_arr = url_query.split('&');
+
+      if (url_search_arr[0].length > 1) {
+          for (i = 0; i < url_search_arr.length; i += 1) {
+              get_param = url_search_arr[i].split("=");
+
+              if (options.unescape) {
+                  key = decodeURI(get_param[0]);
+                  val = decodeURI(get_param[1]);
+              } else {
+                  key = get_param[0];
+                  val = get_param[1];
+              }
+
+              if (options.convert_num) {
+                  if (val.match(/^\d+$/)) {
+                      val = parseInt(val, 10);
+                  } else if (val.match(/^\d+\.\d+$/)) {
+                      val = parseFloat(val);
+                  }
+              }
+
+              if (url_get_params[key] === undefined) {
+                  url_get_params[key] = val;
+              } else if (typeof url_get_params[key] === "string") {
+                  url_get_params[key] = [url_get_params[key], val];
+              } else {
+                  url_get_params[key].push(val);
+              }
+
+              get_param = [];
+          }
+      }
+
+      urlObj = {
+          protocol: a.protocol,
+          hostname: a.hostname,
+          host: a.host,
+          port: a.port,
+          hash: a.hash.substr(1),
+          pathname: a.pathname,
+          search: a.search,
+          parameters: url_get_params
+      };
+
+      return urlObj;
+  }
+
+  /* Ajax DEL post */
+  $('.delbtn').on('click',function(){
+    console.log('Click');
+    var postid = this.getAttribute("data-id");
+    console.log(postid);
+    $.ajax({
+      url: '/post/'+postid,
+      type: 'DELETE',
+      success: function(response) {
+        $('body').removeClass('modal-open');
+        $('#post-'+postid).hide();
+        $('#modal-'+postid).hide();
+      }
+    });
+  });
+
+  //Parse URL
+  var url = window.location.href;
+  var urlObj = urlObject({'url':url});
+
+  //Ajax Order
+  $('.orderbtn').on('click',function(){
+    var order = this.getAttribute("data-name");
+    if(urlObj.parameters.course_name || urlObj.parameters.catalog || urlObj.parameters.qweryw || urlObj.parameters.teacher || urlObj.parameters.order){
+      if(urlObj.parameters.order){
+        var searchword="?";
+        for(var i in urlObj.parameters){
+          if(i!="order"){
+            searchword= searchword+i+"="+urlObj.parameters[i]+"&";
+          }
         }
-    }
-
-    a.href = options.url;
-    url_query = a.search.substring(1);
-    url_search_arr = url_query.split('&');
-
-    if (url_search_arr[0].length > 1) {
-        for (i = 0; i < url_search_arr.length; i += 1) {
-            get_param = url_search_arr[i].split("=");
-
-            if (options.unescape) {
-                key = decodeURI(get_param[0]);
-                val = decodeURI(get_param[1]);
-            } else {
-                key = get_param[0];
-                val = get_param[1];
-            }
-
-            if (options.convert_num) {
-                if (val.match(/^\d+$/)) {
-                    val = parseInt(val, 10);
-                } else if (val.match(/^\d+\.\d+$/)) {
-                    val = parseFloat(val);
-                }
-            }
-
-            if (url_get_params[key] === undefined) {
-                url_get_params[key] = val;
-            } else if (typeof url_get_params[key] === "string") {
-                url_get_params[key] = [url_get_params[key], val];
-            } else {
-                url_get_params[key].push(val);
-            }
-
-            get_param = [];
+        var ajaxurl = "/" + searchword + "order=" + order;
+      }
+      else{
+        var searchword="?";
+        for(var i in urlObj.parameters){
+          if(i!="order"){
+            searchword= searchword+i+"="+urlObj.parameters[i]+"&";
+          }
         }
+        var ajaxurl = "/" + searchword + "&order=" + order;
+      }
     }
+    else{
+      var ajaxurl = "/" + "?order=" + order;
+    }
+    $.ajax({
+      url: ajaxurl,
+      type: 'GET',
+      success: function(response) {
+        $('.post-row').fadeOut().remove();
+        for(var i in response){
+          var dom ="<tr id='post-"+response[i].id+"' class='post-row' data-toggle='modal' data-target='#modal-"+response[i].id+"'><td class='course_name'>"+response[i].course_name+"</td><td class='catalog'>"+response[i].catalog+"</td><td class='teacher'>"+response[i].teacher+"</td><td class='semester'>"+response[i].semester+"</td></tr>";
+          $('#post-table').append(dom);
+        }
+        $('.chevron.down.icon').remove();
+        var icon = "<i class='chevron down icon'></i>";
+        $("th.orderbtn."+order).append(icon);
+      }
+    });
+  });
 
-    urlObj = {
-        protocol: a.protocol,
-        hostname: a.hostname,
-        host: a.host,
-        port: a.port,
-        hash: a.hash.substr(1),
-        pathname: a.pathname,
-        search: a.search,
-        parameters: url_get_params
-    };
-
-    return urlObj;
-}
+});
--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var db = require('../model/db');
 router.get('/', function(req, res) {
   console.log("GET '/'");
   console.log(req.user);
-  if(req.query.hasOwnProperty("order")){
+  if(req.query.order){
     var order = req.query.order;
   }
   else{
@@ -15,93 +15,109 @@ router.get('/', function(req, res) {
   db.GetAll('post',order,function(datas){
     if(req.query.hasOwnProperty("queryw")){
 	  	db.query_post(datas, req.query.queryw,"query",function(data,teachers,course_name){
-        res.render('post/index',{
-          'data':data,
-          'teachers': teachers,
-          'course_name': course_name,
-          'user': req.user
-        });
+        if(req.query.order){
+          res.send(data);
+        }
+        else{
+          res.render('post/index',{
+            'data':data,
+            'teachers': teachers,
+            'course_name': course_name,
+            'user': req.user
+          });
+        }
       });
 	  }
 	  else if(req.query.hasOwnProperty("teacher")){
 	  	db.query_post(datas, req.query.teacher, "teacher",function(data,teachers,course_name){
-        res.render('post/index',{
-          'data':data,
-          'teachers': teachers,
-          'course_name': course_name,
-          'user': req.user
-        });
+        if(req.query.order){
+          res.send(data);
+        }
+        else{
+          res.render('post/index',{
+            'data':data,
+            'teachers': teachers,
+            'course_name': course_name,
+            'user': req.user
+          });
+        }
       });
 	  }
 	  else if(req.query.hasOwnProperty("course_name")){
 	  	db.query_post(datas, req.query.course_name, "course_name",function(data,teachers,course_name){
-        res.render('post/index',{
-          'data':data,
-          'teachers': teachers,
-          'course_name': course_name,
-          'user': req.user
-        });
+        if(req.query.order){
+          res.send(data);
+        }
+        else{
+          res.render('post/index',{
+            'data':data,
+            'teachers': teachers,
+            'course_name': course_name,
+            'user': req.user
+          });
+        }
       });
 	  }
 	  else if(req.query.hasOwnProperty("catalog")){
 	  	switch(req.query.catalog){
 	  		case "A9":
-	  			req.query.catalog = "通識"
+	  			req.query.catalog = "通識";
 	  			break;
 	  		case "A2":
-	  			req.query.catalog = "體育"
+	  			req.query.catalog = "體育";
 	  			break;
 	  		case "A1":
-	  			req.query.catalog = "外國語言"
+	  			req.query.catalog = "外國語言";
 	  			break;
 	  		case "A6":
-	  			req.query.catalog = "服務學習"
+	  			req.query.catalog = "服務學習";
 	  			break;
 	  		case "A7":
-	  			req.query.catalog = "基礎國文"
+	  			req.query.catalog = "基礎國文";
 	  			break;
 	  		case "AG":
-	  			req.query.catalog = "公民歷史"
+	  			req.query.catalog = "公民歷史";
 	  			break;
         case "M":
-          req.query.catalog = "必修"
+          req.query.catalog = "必修";
           break;
         case "C":
-          req.query.catalog = "選修"
+          req.query.catalog = "選修";
           break;
         default :
-          req.query.catalog = ""
+          req.query.catalog = "";
           break;
 	  	}
-	  	db.query_post(datas, req.query.catalog, "catalog",function(data,teachers,course_name){
-        res.render('post/index',{
-          'data':data,
-          'teachers': teachers,
-          'course_name': course_name,
-          'user': req.user
-        });
+	  	db.query_post(datas, req.query.catalog,"catalog",function(data,teachers,course_name){
+        if(req.query.order){
+          res.send(data);
+        }
+        else{
+          res.render('post/index',{
+            'data':data,
+            'teachers': teachers,
+            'course_name': course_name,
+            'user': req.user
+          });
+        }
       });
 	  }
 	  else{
 			var teacher = db.search_item(datas, "teacher");
 		  var courseName = db.search_item(datas, "course_name");
-
-	    res.render('post/index',{
-	    	'data':datas,
-	    	'teachers': teacher,
-	    	'course_name': courseName,
-        'user': req.user
-	    });
+      if(req.query.order){
+        res.send(datas);
+      }
+      else{
+  	    res.render('post/index',{
+  	    	'data':datas,
+  	    	'teachers': teacher,
+  	    	'course_name': courseName,
+          'user': req.user
+  	    });
+      }
 	  }
   });
-});
-
-router.get('/signup', function(req, res) {
-  res.redirect('/user/signup');
-});
-
-router.get('/login', function(req, res) {
-  res.redirect('/user/login');
 });
 
 router.post('/report', function(req, res) {

ae2bcef3378495b8d3033a2f066b270156faf453
BoTung <bver49@gmail.com>
2016-08-23 20:44:25 +0800

    fix callback url

--- model/passport.js
+++ model/passport.js
@@ -9,7 +9,7 @@ var db = require('./db');
 passport.use(new FacebookStrategy({
     clientID: config.fbappid,
     clientSecret: config.fbsecret,
-    callbackURL:"http://localhost:3000/user/auth/facebook/callback",//"http://nckuhub.com/user/auth/facebook/callback",
+    callbackURL:"http://nckuhub.com/user/auth/facebook/callback",
     profileFields: ['id', 'displayName']
   },
   function(accessToken, refreshToken, profile, cb){

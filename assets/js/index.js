2ab473d8f61c6557e3d705e0fc5a5731e16cc505
BoTung <bver49@gmail.com>
2017-02-07 00:36:22 +0800

    change new post to single page

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,22 +9,6 @@ $(document).ready(function(){
     "extendedTimeOut": "1000"
   }
 
-  $('#new').on('shown.bs.modal', function (){
-    var ajaxurl = '/post/new';
-    $.ajax({
-      url:ajaxurl,
-      type: 'GET',
-      success: function(response) {
-        $('#new .modal-content').append(response);
-        $('#new').data('bs.modal').handleUpdate();
-      }
-    });
-  });
-
-  $('#new').on('hidden.bs.modal', function () {
-    $('#new .modal-content').empty();
-  });
-
 });
 
   //Parse URL function

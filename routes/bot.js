23611c49b40e0da867af4aba12d57d05bec63af3
BoTung <bver49@gmail.com>
2017-02-09 02:22:15 +0800

    bot

--- routes/bot.js
+++ routes/bot.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
 var db = require('../model/db');
 
@@ -11,7 +12,7 @@ router.get('/webhook/', function(req, res) {
 });
 
 
-app.post('/webhook/', function(req, res) {
+router.post('/webhook/', function(req, res) {
   messaging_events = req.body.entry[0].messaging
   for (i = 0; i < messaging_events.length; i++) {
     event = req.body.entry[0].messaging[i]

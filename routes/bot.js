60d8f587ed8497a805557236a56fcccfda048626
BoTung <bver49@gmail.com>
2017-02-09 02:20:29 +0800

    bot test

---
+++ routes/bot.js
@@ -0,0 +1,151 @@
+var express = require('express');
+var router = express.Router();
+var db = require('../model/db');
+
+
+router.get('/webhook/', function(req, res) {
+  if (req.query['hub.verify_token'] === 'nckuhubbver49') {
+    res.send(req.query['hub.challenge'])
+  }
+  res.send('Error, wrong token')
+});
+
+
+app.post('/webhook/', function(req, res) {
+  messaging_events = req.body.entry[0].messaging
+  for (i = 0; i < messaging_events.length; i++) {
+    event = req.body.entry[0].messaging[i]
+    sender = event.sender.id
+    if (event.message && event.message.text) {
+      text = event.message.text
+      if (text === 'hi') {
+        sendGenericMessage(sender)
+        continue
+      }
+      sendTextMessage(sender, "parrot: " + text.substring(0, 200))
+    }
+    if (event.postback) {
+      text = JSON.stringify(event.postback)
+      sendTextMessage(sender, "Postback received: " + text.substring(0, 200), token)
+      continue
+    }
+  }
+  res.sendStatus(200)
+})
+
+var token = "EAAUCXLsXLngBACdMmJX3pW7ux5qJuwazfLrOm16rZAyCGqT7pjpDglTaIUfQWqZAC7r5HJudsmGv6BAqfUvhdLawSSI1HNtlKU2465DWwjMhpzkrag2SfLrbgo3qE7qfSZALChe8otEvLN795VmtVftdLMlPvwZA70tJqsSfrAZDZD"
+
+// function to echo back messages - added by Stefan
+
+function sendTextMessage(sender, text) {
+  messageData = {
+    text: text
+  }
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {
+      access_token: token
+    },
+    method: 'POST',
+    json: {
+      recipient: {
+        id: sender
+      },
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending messages: ', error)
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error)
+    }
+  })
+}
+
+
+// Send an test message back as two cards.
+
+function sendGenericMessage(sender) {
+  messageData = {
+    "attachment": {
+      "type": "template",
+      "payload": {
+        "template_type": "generic",
+        "elements": [{
+          "title": "Ai Chat Bot Communities",
+          "subtitle": "Communities to Follow",
+          "image_url": "http://1u88jj3r4db2x4txp44yqfj1.wpengine.netdna-cdn.com/wp-content/uploads/2016/04/chatbot-930x659.jpg",
+          "buttons": [{
+            "type": "web_url",
+            "url": "https://www.facebook.com/groups/aichatbots/",
+            "title": "FB Chatbot Group"
+          }, {
+            "type": "web_url",
+            "url": "https://www.reddit.com/r/Chat_Bots/",
+            "title": "Chatbots on Reddit"
+          }, {
+            "type": "web_url",
+            "url": "https://twitter.com/aichatbots",
+            "title": "Chatbots on Twitter"
+          }],
+        }, {
+          "title": "Chatbots FAQ",
+          "subtitle": "Aking the Deep Questions",
+          "image_url": "https://tctechcrunch2011.files.wordpress.com/2016/04/facebook-chatbots.png?w=738",
+          "buttons": [{
+            "type": "postback",
+            "title": "What's the benefit?",
+            "payload": "Chatbots make content interactive instead of static",
+          }, {
+            "type": "postback",
+            "title": "What can Chatbots do",
+            "payload": "One day Chatbots will control the Internet of Things! You will be able to control your homes temperature with a text",
+          }, {
+            "type": "postback",
+            "title": "The Future",
+            "payload": "Chatbots are fun! One day your BFF might be a Chatbot",
+          }],
+        }, {
+          "title": "Learning More",
+          "subtitle": "Aking the Deep Questions",
+          "image_url": "http://www.brandknewmag.com/wp-content/uploads/2015/12/cortana.jpg",
+          "buttons": [{
+            "type": "postback",
+            "title": "AIML",
+            "payload": "Checkout Artificial Intelligence Mark Up Language. Its easier than you think!",
+          }, {
+            "type": "postback",
+            "title": "Machine Learning",
+            "payload": "Use python to teach your maching in 16D space in 15min",
+          }, {
+            "type": "postback",
+            "title": "Communities",
+            "payload": "Online communities & Meetups are the best way to stay ahead of the curve!",
+          }],
+        }]
+      }
+    }
+  }
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {
+      access_token: token
+    },
+    method: 'POST',
+    json: {
+      recipient: {
+        id: sender
+      },
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending messages: ', error)
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error)
+    }
+  })
+}
+
+
+module.exports = router;

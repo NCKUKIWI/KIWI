ff4d44f634bb1c0add1356eaef511b7311926d95
BoTung <bver49@gmail.com>
2017-02-09 14:36:27 +0800

    fix bug

--- routes/bot.js
+++ routes/bot.js
@@ -113,7 +113,7 @@ function sendGenericMessage(sender) {
 
 function sendCoursePlace(sender,keyword) {
   var db = new dbsystem();
-  db.select().field(["系所名稱","課程名稱","時間","教室"]).where("課程名稱 LIKE '%" + keyword + "%'").run(function(course){
+  db.select().field(["系所名稱","課程名稱","時間","教室"]).from("course_105_2").where("課程名稱 LIKE '%" + keyword + "%'").run(function(course){
     messageData = {
       "attachment":{
         "type": "template",

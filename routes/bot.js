9fb20eb8d1a4fa77a7965a7fecd7494a8ce5d681
BoTung <bver49@gmail.com>
2017-02-12 14:38:06 +0800

    fix notify bug

--- routes/bot.js
+++ routes/bot.js
@@ -533,7 +533,7 @@ function checkCoureseCredit(){
         sendCreditNotify(follow[i]);
       }
       else if (follow[i].餘額!="額滿" && follow[i].count != 0){
-        if(follow[i].count==2){
+        if(follow[i].count==3){
           var count = 0;
         }
         else{
@@ -551,6 +551,13 @@ function checkCoureseCredit(){
 function sendCreditNotify(course){
   var text = "餘額通知！\n\n"+course.content+"／"+course.teacher+"／"+course.time+"。\n\n這門課有 "+course.餘額+" 個餘額了！趕快去選吧 🏄🏄";
   sendTextMessage(course.fb_id,text);
+  if(course.count==3){
+    var count = 0;
+  }
+  else{
+    var count = course.count+1;
+  }
+  db.update().table("follow").set({count:count}).where("id=",course.id).run(function(result){});
 }
 
 setInterval(function(){

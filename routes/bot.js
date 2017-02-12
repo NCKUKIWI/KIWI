89ad951c47f4bb2784a1ba48f8dc51b8557885dc
BoTung <bver49@gmail.com>
2017-02-13 01:06:47 +0800

    fix bug

--- routes/bot.js
+++ routes/bot.js
@@ -13,12 +13,12 @@ router.get('/webhook/', function(req, res) {
 });
 
 router.post('/webhook/', function(req, res) {
-  messaging_events = req.body.entry[0].messaging
+  var messaging_events = req.body.entry[0].messaging
   for (i = 0; i < messaging_events.length; i++) {
-    event = req.body.entry[0].messaging[i]
-    sender = event.sender.id
+    var event = req.body.entry[0].messaging[i]
+    var sender = event.sender.id
     if (event.message && event.message.text) {
-      text = event.message.text
+      var text = event.message.text
       if (text === '小幫手') {
         sendHelloMessage(sender);
         continue;

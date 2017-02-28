5fc7697b5a895f13e442ea987eec188c47c56c86
BoTung <bver49@gmail.com>
2017-02-28 22:40:01 +0800

    fix bug

--- routes/bot.js
+++ routes/bot.js
@@ -559,17 +559,17 @@ function sendCreditNotify(course){
 
 function broadcast(){
   var db = new dbsystem();
-  db.select().field("distinct fb_id").from("follow").run(function(user){
-    for(var i in user){
-      db.select().field(["f.*","c.系號"]).from("follow f").join("course_105_2 c").where("c.id=f.course_id").where("f.fb_id=",user[i].fb_id).where("c.系號!=","A9").run(function(follow){
+  db.select().field("distinct fb_id").from("follow").run(function(users){
+    users.forEach(function(user){
+      db.select().field(["f.*","c.系號"]).from("follow f").join("course_105_2 c").where("c.id=f.course_id").where("f.fb_id=",user.fb_id).where("c.系號!=","A9").run(function(follows){
         var courseinfo="";
-        for(var j in follow ){
-          courseinfo+= (follow[j].serial+"／"+follow[j].content+"\n");
-        }
+        follows.forEach(function(follow){
+          courseinfo+= (follow.serial+"／"+follow.content+"\n");
+        });
         var msg ="同學您好：）\n\n本訊息為小幫手的自動通知，\n提醒您三階選課就在明天囉！\n\n由於本階段通識採用抽籤方式，系統已為大家取消通識餘額的追蹤狀態，避免通知不斷跳出。\n\n-\n\n扣除通識之後，您目前追蹤中的課程如下：\n\n"+courseinfo+"\n若要取消或者增加追蹤，請再次呼喚小幫手即可。\n\n-\n\n第三階段選課：\n3/1 (09:00) ~ 3/6 (17:00)\n預祝各位選課順利：）";
-        sendTextMessage(user[i].fb_id,msg);
+        sendTextMessage(user.fb_id,msg);
       });
-    }
+    });
   });
 }
 

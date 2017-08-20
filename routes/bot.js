fb5396167beab0d4eb18d373da340a1f327373b2
BoTung <bver49@gmail.com>
2017-08-21 00:14:55 +0800

    update bot

--- routes/bot.js
+++ routes/bot.js
@@ -40,7 +40,6 @@ router.post('/webhook/', function(req, res) {
   var messaging_events = req.body.entry[0].messaging
   for (i = 0; i < messaging_events.length; i++) {
     var event = req.body.entry[0].messaging[i]
-    console.log(event);
     var sender = event.sender.id  //使用者messenger id
     if (event.message && event.message.text) {
       console.log("sender: "+sender);

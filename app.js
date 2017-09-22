2cc0bcca599f02aa63397aa893baaad36895f886
Derek Chen <bver49@gmail.com>
2017-09-22 13:21:02 +0800

    add helmet

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var app = express();
 var engine = require("ejs-locals");             //讓express支援layout
 var path = require("path");
 var bodyParser = require("body-parser");
@@ -8,26 +9,22 @@ var db = require("./model/db");
 var flash = require("express-flash");
 var compression = require("compression");
 var cookieParser = require("cookie-parser");
-// 引入router檔案位於routes資料夾中
-var index = require("./routes/index");
-var post = require("./routes/post");
-var user = require("./routes/user");
-var schedule = require("./routes/schedule");
-var course = require("./routes/course");
-var course_rate = require("./routes/course_rate");
-var bot = require("./routes/bot");
-
-var app = express();
+var helmet = require("helmet");
 
 app.engine("ejs", engine);
 app.set("views",path.join(__dirname,"views"));  //view的路徑位在資料夾views中
-app.set("view engine","ejs");                   //使用ejs作為template
+app.set("view engine","ejs");//使用ejs作為template
+
+app.use(helmet());
+app.use(flash());
+app.use(expressValidator());
+app.use(compression());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(require('express-status-monitor')());
-app.use("/assets",express.static("assets",{ maxAge: 30*60*1000 }));
-
+app.use("/assets",express.static("assets",{ maxAge: 24*60*60 }));
+app.use(cookieParser("secretString"));
 //Handle sessions and cookie
 app.use(session({
   cookie: { maxAge: 1000 * 60 * 60 * 24 * 30 },
@@ -35,7 +32,6 @@ app.use(session({
   resave: true,
   saveUninitialized: true,
 }));
-app.use(cookieParser("secretString"));
 
 app.use(function(req, res, next) {
   if(req.cookies.isLogin){
@@ -49,18 +45,14 @@ app.use(function(req, res, next) {
   }
 });
 
-app.use(flash());
-app.use(expressValidator());
-app.use(compression());
-
 //Route
-app.use("/course",course);                              // get "/"時交給routes course
-app.use("/post", post);                          // get "/post"時交給routes post處理
-app.use("/user",user);                          // get "/user"時交給routes user處理
-app.use("/schedule",schedule);                 // get "/schedule"時交給routes schedule
-app.use("/course_rate",course_rate);          // get "/course_rate"時交給routes course_rate
-app.use("/bot",bot);
-app.use("/*",course);
+app.use("/course",require("./routes/course"));                              // get "/"時交給routes course
+app.use("/post",require("./routes/post"));                          // get "/post"時交給routes post處理
+app.use("/user",require("./routes/user"));                          // get "/user"時交給routes user處理
+app.use("/schedule",require("./routes/schedule"));                 // get "/schedule"時交給routes schedule
+app.use("/course_rate",require("./routes/course_rate"));          // get "/course_rate"時交給routes course_rate
+app.use("/bot",require("./routes/bot"));
+app.use("/*",require("./routes/course"));
 
 app.listen( process.env.PORT || 3000);                             //監聽3000port
 console.log("running on port 3000");

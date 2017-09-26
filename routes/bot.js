119722777e5546ef5e885117330699902522f749
Derek Chen <bver49@gmail.com>
2017-09-26 12:28:14 +0800

    fix bot

--- routes/bot.js
+++ routes/bot.js
@@ -92,7 +92,7 @@ router.post('/webhook/', function(req, res) {
 			if (text.indexOf("小幫手") != -1) {
 				sendHelloMessage(sender);
 			} else {
-				var serial = text.replace(/\s\-/g, "").match(/^[a-zA-Z][0-9]{4}/i);
+				var serial = text.replace(/[\s|\-]/g, "").match(/^[a-zA-Z][0-9]{4}/i);
 				if (serial) {
 					if (courseSerialList.indexOf(serial[0].toUpperCase()) !== -1) {
 						askPlaceOrFollow(sender, serial[0]);

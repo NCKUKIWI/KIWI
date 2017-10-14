3782ded9b7ec3c742cd517eff5804693d45bc0c1
BoTung <bver49@gmail.com>
2017-10-14 17:29:38 +0800

    change session to cookie

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-var engine = require("ejs-locals");             //讓express支援layout
+var engine = require("ejs-locals"); //讓express支援layout
 var path = require("path");
 var bodyParser = require("body-parser");
 var expressValidator = require("express-validator");
@@ -12,8 +12,8 @@ var cookieParser = require("cookie-parser");
 var helmet = require("helmet");
 
 app.engine("ejs", engine);
-app.set("views",path.join(__dirname,"views"));  //view的路徑位在資料夾views中
-app.set("view engine","ejs");//使用ejs作為template
+app.set("views", path.join(__dirname, "views")); //view的路徑位在資料夾views中
+app.set("view engine", "ejs"); //使用ejs作為template
 
 app.use(helmet());
 app.use(flash());
@@ -21,38 +21,44 @@ app.use(expressValidator());
 app.use(compression());
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(require('express-status-monitor')());
-app.use("/assets",express.static("assets",{ maxAge: 24*60*60 }));
+app.use("/assets", express.static("assets", {
+    maxAge: 24 * 60 * 60
+}));
 app.use(cookieParser("secretString"));
 //Handle sessions and cookie
 app.use(session({
-  cookie: { maxAge: 1000 * 60 * 60 * 12 },
-  secret:"secret",
-  saveUninitialized:true,
-  resave: true
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 12
+    },
+    secret: "secret",
+    saveUninitialized: true,
+    resave: true
 }));
 
 app.use(function(req, res, next) {
-  if(req.session.isLogin){
-    db.FindbyID("user",req.session.userid,function(user){
-      req.user = user;
-      next();
-    });
-  }
-  else {
-    next();
-  }
+    if (req.cookies.isLogin) {
+        db.FindbyID("user", req.cookies.id, function(user) {
+            req.user = user;
+            next();
+        });
+    }
+    else {
+        next();
+    }
 });
 
 //Route
-app.use("/course",require("./routes/course"));                              // get "/"時交給routes course
-app.use("/post",require("./routes/post"));                          // get "/post"時交給routes post處理
-app.use("/user",require("./routes/user"));                          // get "/user"時交給routes user處理
-app.use("/schedule",require("./routes/schedule"));                 // get "/schedule"時交給routes schedule
-app.use("/course_rate",require("./routes/course_rate"));          // get "/course_rate"時交給routes course_rate
-app.use("/bot",require("./routes/bot"));
-app.use("/*",require("./routes/course"));
+app.use("/course", require("./routes/course")); // get "/"時交給routes course
+app.use("/post", require("./routes/post")); // get "/post"時交給routes post處理
+app.use("/user", require("./routes/user")); // get "/user"時交給routes user處理
+app.use("/schedule", require("./routes/schedule")); // get "/schedule"時交給routes schedule
+app.use("/course_rate", require("./routes/course_rate")); // get "/course_rate"時交給routes course_rate
+app.use("/bot", require("./routes/bot"));
+app.use("/*", require("./routes/course"));
 
-app.listen( process.env.PORT || 3000);                             //監聽3000port
+app.listen(process.env.PORT || 3000); //監聽3000port
 console.log("running on port 3000");
--- routes/user.js
+++ routes/user.js
@@ -5,66 +5,64 @@ var db = require('../model/db');
 var graph = require("fbgraph");
 var config = require('../config');
 
-router.get("/fblogin",helper.checkLogin(1),function(req, res) {
-  res.redirect(`https://www.facebook.com/v2.8/dialog/oauth?client_id=${config.fbappid}&scope=email,public_profile&response_type=code&redirect_uri=${config.website}/user/fbcheck`);
+router.get("/fblogin", helper.checkLogin(1), function(req, res) {
+    res.redirect(`https://www.facebook.com/v2.8/dialog/oauth?client_id=${config.fbappid}&scope=email,public_profile&response_type=code&redirect_uri=${config.website}/user/fbcheck`);
 });
 
-router.get("/fbcheck",helper.checkLogin(1),function(req,res) {
-  if(req.query.code){
-    graph.authorize({
-      "client_id": config.fbappid,
-      "redirect_uri": config.website+"/user/fbcheck",
-      "client_secret": config.fbsecret,
-      "code": req.query.code
-    }, function (err,result) {
-      graph.get(`/me?fields=id,name,email,gender&access_token=${result.access_token}`,function(err,fb){
-        db.FindbyColumn('user',['id','fb_id'],{'fb_id':fb.id},function(user){
-          if(user.length>0){
-            req.session.isLogin = 1;
-            req.session.userid = user[0].id;
-            res.redirect("/");
-          }
-          else{
-            db.Insert('user',{'name':fb.name,'fb_id':fb.id,'role':0,'department':'無','grade':'無'},function (err,result) {
-              if (err) console.log(err);
-              req.session.isLogin = 1;
-              req.session.userid = result.insertId;
-              res.redirect("/");
-            })
-          }
+router.get("/fbcheck", helper.checkLogin(1), function(req, res) {
+    if (req.query.code) {
+        graph.authorize({
+            "client_id": config.fbappid,
+            "redirect_uri": config.website + "/user/fbcheck",
+            "client_secret": config.fbsecret,
+            "code": req.query.code
+        }, function(err, result) {
+            graph.get(`/me?fields=id,name,email,gender&access_token=${result.access_token}`, function(err, fb) {
+                db.FindbyColumn('user', ['id', 'fb_id'], { 'fb_id': fb.id }, function(user) {
+                    if (user.length > 0) {
+                        res.cookie("isLogin", 1, { maxAge: 60 * 60 * 1000 });
+                        res.cookie("id", user[0].id, { maxAge: 60 * 60 * 1000 });
+                        res.redirect("/");
+                    } else {
+                        db.Insert('user', { 'name': fb.name, 'fb_id': fb.id, 'role': 0, 'department': '無', 'grade': '無' }, function(err, result) {
+                            if (err) console.log(err);
+                            res.cookie("isLogin", 1, { maxAge: 60 * 60 * 1000 });
+                            res.cookie("id", result.insertId, { maxAge: 60 * 60 * 1000 });
+                            res.redirect("/");
+                        })
+                    }
+                });
+            });
         });
-      });
+    } else {
+        res.redirect("/");
+    }
+});
+
+router.get('/logout', function(req, res) {
+    res.clearCookie("isLogin");
+    res.clearCookie("id");
+    res.redirect('/');
+});
+
+router.get('/edit', helper.checkLogin(), function(req, res) {
+    console.log('\n' + 'GET /user/edit');
+    res.render('user/edit', {
+        'user': req.user
     });
-  }
-  else{
-    res.redirect("/");
-  }
 });
 
-router.get('/logout',function(req, res){
-    req.session.destroy(function(err) {
-        res.redirect('/');
+router.post('/update', helper.checkLogin(), function(req, res) {
+    console.log('\n' + 'POST /user/update');
+    var userid = req.user.id;
+    var user = {
+        name: req.body.name,
+        department: req.body.department,
+        grade: req.body.grade
+    }
+    db.Update('user', user, { 'id': parseInt(userid) }, function(results) {
+        res.send('success');
     });
-});
-
-router.get('/edit',helper.checkLogin(),function(req, res){
-  console.log('\n'+'GET /user/edit');
-  res.render('user/edit',{
-    'user':req.user
-  });
-});
-
-router.post('/update',helper.checkLogin(),function(req, res){
-  console.log('\n'+'POST /user/update');
-  var userid = req.user.id;
-  var user = {
-    name:req.body.name,
-    department:req.body.department,
-    grade:req.body.grade
-  }
-  db.Update('user',user,{'id':parseInt(userid)},function(results){
-    res.send('success');
-  });
 });
 
 module.exports = router;

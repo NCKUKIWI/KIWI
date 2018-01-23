36c1b81abc3d43dfa2d40366a6809161d76487b2
Derek <bver49@gmail.com>
2018-01-23 21:00:59 +0800

    Update

--- test.js
+++
@@ -1,5 +0,0 @@
-var dbsys = require('./model/dba');
-var db = new dbsys();
-db.select().field(["系所名稱", "課程名稱", "時間", "餘額", "選課序號", "老師"]).from("course_new").where("id=18914").run(function (course) {
-  console.log(course);
-});

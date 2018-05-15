ccec453c4bd0f52b6412954df99ad8fcee3c460f
Benjamin Chen <s910190benjamin@gmail.com>
2018-05-16 04:42:40 +0800

    undo cache.js modify

--- helper/cache.js
+++ helper/cache.js
@@ -1,10 +1,10 @@
 var redis = require("redis"),
 
-    client = redis.createClient({
-        'host': '172.104.161.5',
-        'port': 6379,
-        'db': 1
-    });
+client = redis.createClient({
+    'host':'redis',
+    'port': 6379,
+    'db': 1
+});
 
 client.on("error", function (err) {
     console.log("Error " + err);

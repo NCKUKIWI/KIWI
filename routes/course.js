eb6988971797b620e7f7382ac3e6bbab36be476f
nick0916 <nickliao2013@gmail.com>
2018-07-06 04:45:38 +0800

    暫時先設每次redis都會重設(我不熟redis Q)

--- routes/course.js
+++ routes/course.js
@@ -188,7 +188,7 @@ router.get('/:id', function (req, res) {
         res.redirect('/');
     } else {
         redis.get(courseCacheKey(id), function (err, reply) {
-            if (reply) {
+            if (false) {
                 var data = JSON.parse(reply);
                 var rates = data.rates;
                 if (req.user && rates.length > 0) {

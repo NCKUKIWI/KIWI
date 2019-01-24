40758c5008191a4367efe55e992442a4edb456f5
nick0916 <nickliao2013@gmail.com>
2019-01-25 05:18:16 +0800

    fix query use first teacher's bug

--- routes/course.js
+++ routes/course.js
@@ -129,7 +129,7 @@ router.get('/:id', function (req, res) {
                             courseInfo[j]++;
                         }
                     }
-                    db.FindbyColumn('course_rate', ["*"], { course_name: courseInfo.課程名稱, teacher: courseInfo.老師 }, function (rates) {
+                    db.FindbyColumn('course_rate', ["*"], { course_name: courseInfo.課程名稱, teacher: courseInfo.老師.split(/\s|\*/g)[0] }, function (rates) {
                         var sweet = 0;
                         var cold = 0;
                         var got = 0;

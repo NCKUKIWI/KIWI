8ee6303b144d09458f46172609dd4a05a7ad975d
hugecat <hugecatopmeow@gmail.com>
2019-01-25 20:44:47 +0800

    fix register ui

--- src/JS/public.js
+++ src/JS/public.js
@@ -38,7 +38,7 @@
     // 頁面顯示狀態
 
     var pageStatus = {
-        initial_tab: 'course',
+        initial_tab: 'register',
         now_tab: '',
         next_tab: '',                           // 若需暫停則暫存
         windows: {

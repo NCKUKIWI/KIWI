3afb3674b8e00600e315ea849f5eed998a3e69be
kuma0416 <a24350845@gmail.com>
2019-07-03 22:35:10 +0800

    Fix the bug about counting the number of comments
    Add the preprocessing for trimming the teacher

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 var db = require("./model/db");
-var update_comment_num = "update course_new set comment_num = (select COUNT(id) from post where post.course_name = course_new.課程名稱 and SUBSTRING(post.teacher, 1, 3) = SUBSTRING(course_new.老師, 1, 3))";
-db.Query(update_comment_num,function(result){
-  console.log(result);
+
+let getCourseNew = "SELECT * FROM course_new"
+db.Query(getCourseNew, function (courseData) {
+  for (r in courseData) {
+    let courseInfo = courseData[r];
+    let teachers = courseInfo["老師"].replace(/　/i, "");
+    let firstTeacher = "%" + teachers.split(/\s|\*/g)[0] + "%";
+    let countPostNumber = "SELECT COUNT(*) ";
+    countPostNumber += "FROM post WHERE teacher like " + "\'" + firstTeacher + "\' ";
+    countPostNumber += "AND course_name = " + "\'" + courseInfo["課程名稱"] + "\'" + " ORDER BY semester DESC";
+
+    db.Query(countPostNumber, function (comment_num) {
+       db.Query("UPDATE course_new SET comment_num="+comment_num[0]["COUNT(*)"]+" WHERE id="+courseInfo['id'],function(){});
+    });
+  }
 });
